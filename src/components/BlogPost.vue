935617ee15dd2c11bfa01393efe9ec7e0a84bc84
hqying <hqying.z@gmail.com>
2018-05-26 10:10:02 +0800

    slot, named slot

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,9 +1,19 @@
 <template>
   <div>
-    {{title}}
-    <button @click="$emit('enlarge-event')">enlarge</button>
-    <button @click="$emit('smaller-event')">smaller</button>
-    <button @click="$emit('event-with-arg', {name: 'name-abc', addr: 'addr room 8003'}, {text: 'second arg'})">event with arg</button>
+    <div>
+      {{title}}
+      <button @click="$emit('enlarge-event')">enlarge</button>
+      <button @click="$emit('smaller-event')">smaller</button>
+      <button @click="$emit('event-with-arg', {name: 'name-abc', addr: 'addr room 8003'}, {text: 'second arg'})">event with arg</button>
+    </div>
+    <div>
+      <p>[slot]</p>
+      <slot></slot>
+    </div>
+    <div>
+      <p>[named slot]</p>
+      <slot name="tail"></slot>
+    </div>
   </div>
 </template>
 
--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,7 +1,10 @@
 <template>
   <div :style="{fontSize: postFontSize + 'px'}">
     <blog-post v-for="post in posts" :title="post" v-on:enlarge-event="postFontSize += 2" v-on:smaller-event="postFontSize -= 2"
-               v-on:event-with-arg="eventWithArgHandler"></blog-post>
+               v-on:event-with-arg="eventWithArgHandler">
+      <span slot="tail">tail slot</span>
+      <p>blablabla slot content</p>
+    </blog-post>
   </div>
 </template>
 
